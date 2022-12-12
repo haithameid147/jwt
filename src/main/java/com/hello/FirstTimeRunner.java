4d03e809a6717ebb98c99ba1391fbdd9c45e1866
DESKTOP-37JEQE4\hp <haitham.muhamad30@gmail.com>
2022-12-12 16:15:54 +0300

    save arezzo files

---
+++ src/main/java/com/hello/FirstTimeRunner.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hello;
+// set global time_zone ="+3:00";
+
+
+// ssss
+import com.hello.sec.AppUser;
+import com.hello.sec.UserService;
+
+import java.util.Date;
+
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class FirstTimeRunner implements CommandLineRunner {
+
+	private final Log log = LogFactory.getLog(FirstTimeRunner.class);
+
+	
+
+           
+	@Autowired
+	private UserService userService ;
+
+	
+	@Override
+	public void run(String... args) throws Exception {
+
+		userService.addUser(new AppUser("admin" , "admin" , true , "admin"));
+     	userService.addUser(new AppUser("user11" , "user11" , true , "user"));
+  
+
+        
+
+	}
+
+}
---
+++ src/main/java/com/hello/MvcConfig.java
@@ -0,0 +1,56 @@
+package com.hello;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestContextListener;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class MvcConfig implements WebMvcConfigurer {
+	
+	public MvcConfig()
+	{
+		super();
+	}
+	public void addViewControllers(ViewControllerRegistry registry) {
+		 registry.addViewController("/test.html");
+	      
+    	
+	}
+	public void adddViewControllers(ViewControllerRegistry registry) {
+		
+		registry.addViewController("/test.html").setViewName("test");
+		
+	}
+	public void addResourceHandlers(final ResourceHandlerRegistry registry)
+	{
+		registry.addResourceHandler("/**",
+        		"/css/**",
+                "/resources/**",
+                "/js/**",
+                "/images/**",
+                "/api/**",
+                "/font-awesome/**"
+               
+               )
+		        .addResourceLocations(
+        		   "classpath:/static/css/",
+        		   "classpath:/static/js/",
+        		   "classpath:/static/images/",
+        		   "classpath:/static/api/",
+                   "classpath:/resources/",
+                   "classpath:/static/font-awesome/");
+	}
+	
+	@Bean
+    @ConditionalOnMissingBean(RequestContextListener.class)
+    public RequestContextListener requestContextListener() {
+        return new RequestContextListener();
+    }
+	
+}
