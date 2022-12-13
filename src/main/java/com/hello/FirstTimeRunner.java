0a4b4660faf8e6a105b71cce73fbe4e1b2dfcaad
DESKTOP-37JEQE4\hp <haitham.muhamad30@gmail.com>
2022-12-13 09:03:11 +0300

    save arezzo filkp

--- src/main/java/com/hello/FirstTimeRunner.java
+++ src/main/java/com/hello/FirstTimeRunner.java
@@ -38,6 +38,9 @@ public class FirstTimeRunner implements CommandLineRunner {
 		userService.addUser(new AppUser("admin" , "admin" , true , "admin"));
      	userService.addUser(new AppUser("user11" , "user11" , true , "user"));
 
+     	
+		userService.addUser(new AppUser("admin11" , "admin11" , true , "admin11"));
+     	userService.addUser(new AppUser("user1122" , "user1122" , true , "user22"));
 	}
 
 }
